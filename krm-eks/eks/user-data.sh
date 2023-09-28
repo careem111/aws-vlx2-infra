fefa01ef9b5c086c0c38e906a5a4ee1fcf6ed586
careem111 <krm.mrt@gmail.com>
2023-09-28 20:04:10 +0530

    add aws creds in jenkins

--- krm-eks/eks/user-data.sh
+++ krm-eks/eks/user-data.sh
@@ -13,8 +13,12 @@ sudo su -
 usermod -aG docker jenkins
 usermod -aG docker ubuntu
 systemctl restart docker
-apt-get install git -y
-git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y && sudo apt install terraform -y
+apt update -y && apt install terraform -y
+apt-get install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+apt-get install git -y
+git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
