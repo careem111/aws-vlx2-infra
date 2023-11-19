e12b9f7a3c27db03c81a75a0de519393629ff291
careem111 <krm.mrt@gmail.com>
2023-11-19 18:02:53 +0530

    modify sg ingress rules

--- krm-eks/eks/sg.tf
+++ krm-eks/eks/sg.tf
@@ -3,25 +3,21 @@ resource "aws_security_group" "node" {
   description = "Allow ssh inbound traffic"
   vpc_id      =  module.eks_cluster_infra.vpc_id
 
-  ingress =  [
-     
-     {
-    description      = "ssh access to public"
+  ingress {description      = "ssh access to public"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  },
+  }
   
-  {description      = "access to the app"
+  ingress {description      = "access to the app"
     from_port        = 30000
     to_port          = 32767
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-      ]
 
-  egress {
+egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
