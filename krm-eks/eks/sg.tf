6b8642d7e92fd42523aac3fbe607bf493e7789fc
careem111 <krm.mrt@gmail.com>
2023-11-19 15:37:02 +0530

    add nodport rule in sg

--- krm-eks/eks/sg.tf
+++ krm-eks/eks/sg.tf
@@ -3,43 +3,23 @@ resource "aws_security_group" "node" {
   description = "Allow ssh inbound traffic"
   vpc_id      =  module.eks_cluster_infra.vpc_id
 
-  ingress {
+  ingress =  [
+     
+     {
     description      = "ssh access to public"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-}
-
-resource "aws_security_group" "jenkins-sg" {
-  name        = "jenkins-sg"
-  description = "Allow ssh inbound traffic"
-  vpc_id      =  module.eks_cluster_infra.vpc_id
-
-  ingress {
-    description      = "ssh access to public"
-    from_port        = 22
-    to_port          = 22
+  },
+  
+  {description      = "access to the app"
+    from_port        = 30000
+    to_port          = 32767
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
-  ingress {
-    description      = "jenkins access to public"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+      ]
 
   egress {
     from_port        = 0
